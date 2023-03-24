f17aefc892410bfd34b20fd0cba010a86b287a2e
Joseph <yousef.aburizek@gmail.com>
2023-03-24 22:24:39 +0300

    Return only user token

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -10,20 +10,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserWithTokenSerializer(serializers.ModelSerializer):
-    # role = serializers.SerializerMethodField(read_only=True)
-    # is_admin = serializers.SerializerMethodField(read_only=True)
     token = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'token']
+        fields = ['token']
 
-    # def get_role(self, obj):
-    #     return obj.user_role.role_name
-    
-    # def get_is_admin(self, obj):
-    #     return obj.is_superuser
-    
     def get_token(self, obj):
         try:
             token = obj.auth_token
@@ -32,3 +24,11 @@ class UserWithTokenSerializer(serializers.ModelSerializer):
             pass
         token = Token.objects.create(user=obj)
         return str(token.key)
+
+
+class UserDetailsSerializer(UserWithTokenSerializer):
+    username = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username']
