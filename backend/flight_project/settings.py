44a818e6a1987f99550a5f9641a5a138b2111bb1
Joseph <yousef.aburizek@gmail.com>
2023-04-28 23:36:37 +0300

    React build and connected to Django

--- backend/flight_project/settings.py
+++ backend/flight_project/settings.py
@@ -83,7 +83,9 @@ ROOT_URLCONF = 'flight_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.parent / 'frontend/build',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -152,11 +154,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR.parent / 'frontend/build/static',
+]
+
+STATIC_ROOT = BASE_DIR.parent / 'frontend/build/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
