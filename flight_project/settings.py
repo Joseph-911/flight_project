09b5ebfa93dd63a223a9930ebd61a5ecc75cfac5
Joseph <yousef.aburizek@gmail.com>
2023-05-04 00:21:22 +0300

    frontend and backend merged

--- flight_project/settings.py
+++ flight_project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-go681_(l=lhufa7t-x2wp92prjw5k4j9ospl#m=2!7@(h=0ozw'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
@@ -83,7 +83,9 @@ ROOT_URLCONF = 'flight_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -159,6 +161,7 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+    BASE_DIR / 'frontend/build/static'
 ]
 
 
