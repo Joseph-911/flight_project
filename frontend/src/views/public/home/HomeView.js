d28045daabaef85ce64b5f40371249ba6dc9bfa1
Joseph <yousef.aburizek@gmail.com>
2023-05-15 18:14:48 +0300

    Home page background

--- frontend/src/views/public/home/HomeView.js
+++ frontend/src/views/public/home/HomeView.js
@@ -1,7 +1,16 @@
 import React from "react";
+import connected_world from "assets/images/backgrounds/connected_world.svg";
 
 const HomeView = () => {
-    return <div>Home</div>;
+    return (
+        <>
+            <div className="hero-wrapper">
+                <div className="hero-cover">
+                    <img src={connected_world} alt="Connected World" />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default HomeView;
